13d5db8c45f539905888484f2dd2d87a1587fcc3
Byambasurenn <byambag12@gmail.com>
2020-04-08 18:37:43 +0800

    Initial commit

---
+++ lib/exhibit_details.dart
@@ -0,0 +1,162 @@
+import 'package:flutter/material.dart';
+
+class ExhibitDetailsPage extends StatelessWidget {
+  static final String path = "lib/exhibit_details.dart";
+  final String image = "assets/exhibit/golden_boots.jpg";
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Stack(
+        children: <Widget>[
+          Container(
+              foregroundDecoration: BoxDecoration(
+                  color: Colors.black26
+              ),
+              height: 400,
+              child: Image.asset(image, fit: BoxFit.fitWidth)),
+          SingleChildScrollView(
+            padding: const EdgeInsets.only(top: 16.0,bottom: 20.0),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: <Widget>[
+                const SizedBox(height: 250),
+                Padding(
+                  padding: const EdgeInsets.symmetric(horizontal:16.0),
+                  child: Text(
+                    "Богд хааны\nАлтан Гутал",
+                    style: TextStyle(color: Colors.white, fontSize: 28.0, fontWeight: FontWeight.bold),
+                  ),
+                ),
+                Row(
+                  children: <Widget>[
+                    const SizedBox(width: 16.0),
+                    Container(
+                      padding: const EdgeInsets.symmetric(
+                        vertical: 8.0,
+                        horizontal: 16.0,
+                      ),
+                      decoration: BoxDecoration(
+                          color: Colors.grey,
+                          borderRadius: BorderRadius.circular(20.0)),
+                      child: Text(
+                        "XX зууны эхэн үе",
+                        style: TextStyle(color: Colors.white, fontSize: 13.0),
+                      ),
+                    ),
+                    Spacer(),
+                    IconButton(
+                      color: Colors.white,
+                      icon: Icon(Icons.favorite_border),
+                      onPressed: () {},
+                    )
+                  ],
+                ),
+                Container(
+                  padding: const EdgeInsets.all(32.0),
+                  color: Colors.white,
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    mainAxisSize: MainAxisSize.min,
+                    children: <Widget>[
+                      Row(
+                        children: <Widget>[
+                          Expanded(
+                            child: Column(
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              children: <Widget>[
+                                Row(
+                                  children: <Widget>[
+                                    Icon(
+                                      Icons.star,
+                                      color: Colors.purple,
+                                    ),
+                                    Icon(
+                                      Icons.star,
+                                      color: Colors.purple,
+                                    ),
+                                    Icon(
+                                      Icons.star,
+                                      color: Colors.purple,
+                                    ),
+                                    Icon(
+                                      Icons.star,
+                                      color: Colors.purple,
+                                    ),
+                                    Icon(
+                                      Icons.star_border,
+                                      color: Colors.purple,
+                                    ),
+                                  ],
+                                ),
+                                Text.rich(TextSpan(children: [
+                                  WidgetSpan(
+                                      child: Icon(Icons.location_on, size: 16.0, color: Colors.grey,)
+                                  ),
+                                  TextSpan(
+                                      text: "Өвлийн ордон"
+                                  )
+                                ]), style: TextStyle(color: Colors.grey, fontSize: 12.0),)
+                              ],
+                            ),
+                          ),
+                        ],
+                      ),
+                      const SizedBox(height: 30.0),
+                      Text("Тайлбар".toUpperCase(), style: TextStyle(
+                          fontWeight: FontWeight.w600,
+                          fontSize: 14.0
+                      ),),
+                      const SizedBox(height: 10.0),
+                      Text(
+                        "Монгол үндэсний хувцас нь төрийн бэлгэдэл утгыг хадгалж явдаг бөгөөд айл гэрийг төр улсын үндэс гэж үзэх тул шинэ айл бүтэхэд үндэсний хувцсаа өмсөж ёсолдог заншилтай.", textAlign: TextAlign.justify, style: TextStyle(
+                          fontWeight: FontWeight.w300,
+                          fontSize: 14.0
+                      ),),
+                      const SizedBox(height: 10.0),
+                      Text(
+                        "Хааны эдэлж хэрэглэж байсан алтадсан савхин гадартай, ширэн ултай энэ гутал нь Монголчуудын дунд Богд хааны алтан гутал хэмээн нэрлэгддэг. \n Эл гутал нь 43,5 см өндөр, 35 см урт.", textAlign: TextAlign.justify, style: TextStyle(
+                          fontWeight: FontWeight.w300,
+                          fontSize: 14.0
+                      ),),
+                    ],
+                  ),
+                ),
+              ],
+            ),
+          ),
+          Positioned(
+            top: 0,
+            left: 0,
+            right: 0,
+            child: AppBar(
+              backgroundColor: Colors.transparent,
+              elevation: 0,
+              centerTitle: true,
+              title: Text("Тайлбар",style: TextStyle(
+                  fontSize: 16.0,
+                  fontWeight: FontWeight.normal
+              ),),
+            ),
+          ),
+          Align(
+            alignment: Alignment.bottomLeft,
+            child: BottomNavigationBar(
+              backgroundColor: Colors.white54,
+              elevation: 0,
+              selectedItemColor: Colors.black,
+              items: [
+                BottomNavigationBarItem(
+                    icon: Icon(Icons.search), title: Text("Хайх")),
+                BottomNavigationBarItem(
+                    icon: Icon(Icons.favorite_border),
+                    title: Text("Нэмэх")),
+                BottomNavigationBarItem(
+                    icon: Icon(Icons.settings), title: Text("Тохиргоо")),
+              ],
+            ),
+          )
+        ],
+      ),
+    );
+  }
+}
